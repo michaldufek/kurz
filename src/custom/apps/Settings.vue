c3a2e8501f839ef463cd0cabf4066eace96c12f5
G <gabriel.staron@gmail.com>
2019-10-18 15:00:19 +0200

    first locale changer try

--- src/custom/apps/Settings.vue
+++ src/custom/apps/Settings.vue
@@ -24,6 +24,12 @@
         </card>
       </div>            
     </div>
+    <div class="locale-changer">
+      <!-- to-do: not working globally now -->
+      <select v-model="$root.$i18n.locale">
+        <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+      </select>
+    </div>
     <base-checkbox style="margin-bottom: 30px;">{{$t('settings.darkMode')}}</base-checkbox>
     <base-button style="margin-left: 20px;" type="secondary" fill>{{$t('settings.save')}}</base-button>        
   </div>
@@ -32,12 +38,18 @@
   export default {
     components: {
     },
-    methods: {
-      changeLocale() {
-          this.$i18n.locale = this.$i18n.locale === 'en' ? 'cs' : 'en'
-          // $root.$i18n.locale
+    data () {
+      return { 
+        langs: [ 'en', 'cs' ] 
       }
     }
+    // methods: {
+      // changeLocale() {
+      //   // to-do: connect to a language switcher radio
+      //   this.$i18n.locale = this.$i18n.locale === 'en' ? 'cs' : 'en'
+      //   // $root.$i18n.locale
+      // }
+    // }
   }
 </script>
 <style>
