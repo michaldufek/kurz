5d1228940984440950debc815ea8206263dbe33e
A Y <gabriel.staron@gmail.com>
2020-01-15 11:12:13 +0100

    menus shouldn't scroll

--- src/custom/layout/application/patternLab/performanceResults/TopNavbar.vue
+++ src/custom/layout/application/patternLab/performanceResults/TopNavbar.vue
@@ -4,9 +4,9 @@
       <div class="navbar-wrapper">
         <div style="margin-top: -10px;">
           <router-link class="navbar-brand" :to="{ name: 'patterns', params: { params } }">{{$t("research.patternLab.backtestPatterns.performanceResults.patterns.title")}}</router-link>
-          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/trades" style="margin-left: 100px;">{{$t("research.patternLab.backtestPatterns.performanceResults.trades.title")}}</router-link>
-          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/performanceMetrics" style="margin-left: 175px;">{{$t("research.patternLab.backtestPatterns.performanceResults.performanceMetrics.title")}}</router-link>
-          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/chart" style="margin-left: 375px;">{{$t("research.patternLab.backtestPatterns.performanceResults.chart.title")}}</router-link>
+          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/trades">{{$t("research.patternLab.backtestPatterns.performanceResults.trades.title")}}</router-link>
+          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/performanceMetrics">{{$t("research.patternLab.backtestPatterns.performanceResults.performanceMetrics.title")}}</router-link>
+          <router-link class="navbar-brand" to="/research/patternLab/backtestPatterns/performanceResults/chart">{{$t("research.patternLab.backtestPatterns.performanceResults.chart.title")}}</router-link>
         </div>
       </div>
     </div>
