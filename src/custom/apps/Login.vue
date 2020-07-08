78306326362c3bdfb4f986ba717386efa231b06f
anly <gabriel.staron@gmail.com>
2020-07-08 19:01:28 +0200

    Revert "get rid of 'CSRF Failed: CSRF token missing or incorrect' problem"
    This reverts commit b833745bc91af807cbdfc72cc5b181f089f8d397.

--- src/custom/apps/Login.vue
+++ src/custom/apps/Login.vue
@@ -209,6 +209,8 @@ export default {
             } 
             this.remember = JSON.parse(localStorage.remember)
 
+            auth.setCSRFToken()
+
             if (process.env.NODE_ENV !== 'production') {
                 console.log('In Dev Mode')
             }
@@ -222,6 +224,7 @@ export default {
                     this.shakeModal()
                     this.message = err
                 } else {
+                    auth.setCSRFToken()
                     this.$store.setItem('headers', {
                         headers: {
                             "Authorization": `Basic ${window.btoa(`${this.email}:${this.pass}`)}`
